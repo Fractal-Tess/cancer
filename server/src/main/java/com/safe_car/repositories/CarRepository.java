d7eae3be47d6146d6f95502542dfe7a91f6c6bd9
PhatPhuckDave <david.majdandzic@hotmail.com>
2025-05-29 18:13:34 +0200

    Remove CarRepository and simplify InsuranceRepository and UserRepository annotations

--- server/src/main/java/com/safe_car/repositories/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.safe_car.repositories;
-
-import com.safe_car.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-} 
