77b2286f33de5036119ce1639e43b7cf8fa17b27
PhatPhuckDave <david.majdandzic@hotmail.com>
2025-05-29 14:46:57 +0200

    Update CORS configuration to use allowedOriginPatterns and allowCredentials

--- server/src/main/java/com/safe_car/config/WebConfig.java
+++ server/src/main/java/com/safe_car/config/WebConfig.java
@@ -7,10 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Allow all origins
-                .allowedMethods("*"); // Allow all HTTP methods
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOriginPatterns("*") // Allow all origins
+				.allowedMethods("*")
+				.allowCredentials(true); // Allow all HTTP methods
+	}
 }
